b71c38b8ae3058c9bea72e967341a42b18f1a068
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-21 14:14:12 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,4 +1,6 @@
 # set env vars
 set -o allexport; source .env; set +o allexport;
 
-chown -R 1001:1001 /opt/app/${PIPELINE_NAME}/
+chown -R 1001:1001 ./bin
+chown -R 1001:1001 ./config
+chown -R 1001:1001 ./www
